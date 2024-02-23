d1940b45e02e29a624818946ababad1ab11d1483
AdiWarke <aditya.warke21@vit.edu>
2024-02-23 14:22:16 +0530

    edited

--- LS_MK_0/Compute_PID.h
+++ LS_MK_0/Compute_PID.h
@@ -1,72 +1,103 @@
 #include <Arduino.h>
 
-double Compute_roll() {
+double roll_offfset = 0, error_roll = 0, roll_setpoint =0;
+double error_yaw = 0;
+double ITerm1, lastInput1, dInput1, ITerm2, lastInput2, dInput2;
+double outputR, outputY;
+float max_angle_enc=2;
+bool STOP_FLAG = true;
 
-  double Kp =0 , Ki = 0, Kd = 0;
+void Compute_roll();
+
+void Compute_yaw();
+
+
+void Compute_roll()
+{
+
+  double Kp = 0, Ki = 0, Kd = 0;
   double roll_angle;
 
-
-    // Obtain the current tilt angle and make a copy to avoid
-	// abrupt changes during current PID computation loop
+  // Obtain the current tilt angle and make a copy to avoid
+  // abrupt changes during current PID computation loop
   roll_angle = roll;
 
-  roll_setpoint = roll_offfset + yaw_output;
+  roll_setpoint = roll_offfset + outputY;
 
   error_roll = roll_setpoint - roll_angle;
 
-  if (abs(error_roll) < 3.0)
+  // Turn motors off if robot falls beyond recoverable angle and await human rescue
+	if (abs(error_roll) >= 75)
 	{
-		Kp = con_KP;
-		Ki = con_KI;
-		Kd = con_KD;
-	}
-	
-	// Aggressive PID gains for |errors| >= 3 degress
-	else
-	{
-		Kp = agr_KP;
-		Ki = agr_KI;
-		Kd = agr_KD;
+		outputR = 0;
+		return;
 	}
 
-  ITerm1 += Ki*(error_roll);
-  
+  if (abs(error_roll) < 3.0)
+  {
+    Kp = roll_con_KP;
+    Ki = roll_con_KI;
+    Kd = roll_con_KD;
+  }
+
+  // Aggressive PID gains for |errors| >= 3 degress
+  else
+  {
+    Kp = roll_agr_KP;
+    Ki = roll_agr_KI;
+    Kd = roll_agr_KD;
+  }
+
+  dInput1 = (roll_angle - lastInput1); // curr - prev
+
+  ITerm1 += Ki * (error_roll);
   ITerm1 = constrain(ITerm1, -255, 255);
 
-  
-  dInput1 = (roll_angle - lastInput1);  // curr - prev
+  outputR = Kp * error_roll + Ki * ITerm1 - Kd * dInput1;
 
-  output1 = Kp * error_roll + Ki * ITerm1 - Kd * dInput1;
-
-  output1 = constrain(output1, -255, 255);
-
+  outputR = constrain(outputR, -255, 255);
 
   lastInput1 = roll_angle;
-  
-  return output1;
+
+  // return outputR;
 }
 
-double Compute_yaw() {
+double Compute_yaw()
+{
 
-    double Kp =0 , Ki = 0, Kd = 0;
-    double yaw_angle;
+  double Kp = 0, Ki = 0, Kd = 0;
+  double yaw_angle;
 
-    yaw_angle = encoder_count();
+  yaw_angle = encoder_count();
 
-    ITerm2 += (error_yaw);
-    if (ITerm2 > outMax) ITerm2 = 255;
-    else if (ITerm2 < outMin) ITerm2 = -255;
+  error_yaw = 0 - yaw_angle;
 
-    if (abs(error_yaw) < 0.05) ITerm2 = 0;
-    dInput2 = (error_yaw - lastInput2) / timeChange * 100;  //Curr - prev.
+  if (!STOP_FLAG)
+  {
+    Kp = yaw_con_KP;
+    Ki = yaw_con_KI;
+    Kd = yaw_con_KD;
+    max_angle_enc = 2;
+  }
+  // Static balance
+  else
+  {
+    Kp = yaw_agr_KP;
+    Ki = yaw_agr_KI;
+    Kd = yaw_agr_KD;
+    max_angle_enc = 2;
+  }
 
-    output2 = Kp * error_yaw + Ki * ITerm2 + Kd * dInput2;  // same signs of kp and kd term.
+  ITerm2 += Ki*(error_yaw);
+  ITerm2 = constrain(ITerm2, -255, 255);
 
-    if (output2 > outMax) output2 = outMax;
-    if (output2 < outMin) output2 = outMin;
-    //      if(output2 < 0) output2 = abs(output2);
+  dInput2 = (yaw_angle - lastInput2); // Curr - prev.
 
-    lastInput2 = error_yaw;
-    lastTime2 = now;
-    return output2;
+  outputY = Kp * error_yaw + Ki * ITerm2 - Kd * dInput2; // same signs of kp and kd term.
+
+  outputY = constrain(outputY, -max_angle_enc, max_angle_enc);
+
+  lastInput2 = yaw_angle;
+ 
+  // return outputY;
 }
